5f46f8e019520facdb25998f459cc8c678a9ed41
Pedro <pedroteixeirabisognin@gmail.com>
2020-07-26 16:00:28 -0300

    Estado detached

--- src/main/java/br/com/alura/jpa/testes/CriaContaComSaldo.java
+++ src/main/java/br/com/alura/jpa/testes/CriaContaComSaldo.java
@@ -8,21 +8,33 @@ import br.com.alura.jpa.modelo.Conta;
 
 public class CriaContaComSaldo {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+        EntityManager em = emf.createEntityManager();
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("contas");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Conta conta = new Conta();
+        conta.setTitular("Márcia");
+        conta.setNumero(12345);
+        conta.setAgencia(54321);
+        conta.setSaldo(100.0);
 
-		Conta conta = new Conta();
-		conta.setTitular("Juliano");
-		conta.setNumero(1234);
-		conta.setAgencia(4321);
-		conta.setSaldo(500.0);
+        em.getTransaction().begin();
 
-		entityManager.getTransaction().begin();
+        em.persist(conta);
 
-		entityManager.persist(conta);
+        em.getTransaction().commit();
+        //Conta está no estado detached
+        em.close();
 
-		entityManager.getTransaction().commit();
-	}
+        //É necessário criar novamente uma instancia de EntityManager
+        EntityManager em2 = emf.createEntityManager();
+        System.out.println("ID da Conta da Márcia:" + conta.getId());
+        conta.setSaldo(500.0);
+
+        em2.getTransaction().begin();
+        //Tira conta do estado detached e a colocar no estado managed novamente
+        em2.merge(conta);
+
+        em2.getTransaction().commit();
+    }
 }
