02a5031c5eeb6c1c9e1e27ff56bad85f650bab73
Pedro <pedroteixeirabisognin@gmail.com>
2020-07-28 23:29:36 -0300

    typedquery e order by desc

--- src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
+++ src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.alura.jpa.modelo.Conta;
 import br.com.alura.jpa.modelo.Movimentacao;
@@ -17,15 +17,16 @@ public class TesteJPQL {
 
         Conta conta = new Conta();
         conta.setId(1L);
-        String jpql = "select m from Movimentacao m where m.conta = :pConta";
+        String sql = "select m from Movimentacao m where m.conta = :pConta order by m.valor desc";
 
-        Query query = em.createQuery(jpql);
+        TypedQuery<Movimentacao> query = em.createQuery(sql, Movimentacao.class);
         query.setParameter("pConta", conta);
-        List<Movimentacao> resultList = query.getResultList();
 
-        for (Movimentacao movimentacao : resultList) {
+        List<Movimentacao> movimentacoes = query.getResultList();
+        for (Movimentacao movimentacao : movimentacoes) {
             System.out.println("Descrição: " + movimentacao.getDescricao());
+            System.out.println("Valor: " + movimentacao.getValor());
             System.out.println("Tipo: " + movimentacao.getTipoMovimentacao());
-    }
+        }
 }
 }
